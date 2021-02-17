1d1d8620c9d3b2670d35e5c7ff54279efc8762af
diego_07 <diego_33_04@hotmail.com>
2021-02-16 23:59:12 -0300

    utilizando la libreria Axio traemos desde Mockapi los arreglos

--- entrega-final-fernandez-figueroa/src/components/Noticias.vue
+++ entrega-final-fernandez-figueroa/src/components/Noticias.vue
@@ -13,13 +13,13 @@
                         <th>Año</th>
                         <th>Acciones</th>
                     </tr>
-                    <tr class="" v-for="persona in lista" :key="persona.id">
-                        <td v-text="persona.Id"></td>
-                        <td v-text="persona.Nombre"></td>
-                        <td v-text="persona.Apellido"></td>
-                        <td v-text="persona.Email"></td>
-                        <td v-text="persona.Tema"></td>
-                        <td v-text="persona.Año"></td>
+                    <tr class="" v-for="persona in datos" :key="persona.id">
+                        <td>{{persona.id}}</td>
+                        <td>{{persona.Nombre}}</td>
+                        <td>{{persona.Apellido}}</td>
+                        <td>{{persona.Email}}</td>
+                        <td>{{persona.Tema}}</td>
+                        <td>{{persona.Anio}}</td>
                         <td class="d-flex justify-content-around table-dark"> <button>Editar</button> <button>Eliminar</button> </td> 
                     </tr>
 
@@ -30,102 +30,30 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                id: 0,
-                lista: [{
-                        Id: "1",
-                        Nombre: "Carlos",
-                        Apellido: "Medina",
-                        Email: "carlitos@blalba.com",
-                        Tema: "Rather Be",
-                        Año: "2014"
-                    }, {
-                        Id: "2",
-                        Nombre: "Juana",
-                        Apellido: "Alberdi",
-                        Email: "coquito@blablba.com",
-                        Tema: "Miss you",
-                        Año: "2000"
+import axios from "axios"
+export default {
+  data() {
+      return {
+          datos:null
+      }
+  },
+  mounted() {
+      this.getDatos();
 
-                    }, {
-                        Id: "3",
-                        Nombre: "Melani",
-                        Apellido: "Cardenas",
-                        Email: "Mela@blalba.com",
-                        Tema: "Real love",
-                        Año: "2000"
+  },
+  methods: {
+      getDatos(){
+          axios
+               .get('https://602c695a30ba720017222ef9.mockapi.io/Datos')
+               .then(response =>{
+                   this.datos = response.data
 
-                    }, {
-                        Id: "4",
-                        Nombre: "Joaquin",
-                        Apellido: "Marredo",
-                        Email: "roaM@blalba.com",
-                        Tema: "Miss You",
-                        Año: "2000"
-
-                    }, {
-                        Id: "5",
-                        Nombre: "Nicolas",
-                        Apellido: "Benavi",
-                        Email: "nico@blalba.com",
-                        Tema: "Rather be",
-                        Año: "2014"
-
-                    }, {
-                        Id: "6",
-                        Nombre: "Rita",
-                        Apellido: "Celeste",
-                        Email: "riti@blalba.com",
-                        Tema: "Symphony",
-                        Año: "2000"
-
-                    }, {
-                        Id: "7",
-                        Nombre: "Federica",
-                        Apellido: "Miguel",
-                        Email: "fede@blalba.com",
-                        Tema: "Real love",
-                        Año: "2001"
-
-                    }, {
-                        Id: "8",
-                        Nombre: "Mirtha",
-                        Apellido: "Mabel",
-                        Email: "mirthi@blalba.com",
-                        Tema: "Mama",
-                        Año: "2012"
-
-                    }, {
-                        Id: "9",
-                        Nombre: "Clara",
-                        Apellido: "Oscura",
-                        Email: "oscucla@blalba.com",
-                        Tema: "Rather Be",
-                        Año: "2014"
-
-                    }, {
-                        Id: "10",
-                        Nombre: "Miguel",
-                        Apellido: "Del Campo",
-                        Email: "miguel@blalba.com",
-                        Tema: "Rather be",
-                        Año: "2014"
-                    }, {
-                        Id: "11",
-                        Nombre: "Oscar",
-                        Apellido: "Clima",
-                        Email: "osqui@blalba.com",
-                        Tema: "Mama",
-                        Año: "2012"
-                    }
-
-
-                ]
-            }
-        },
-    }
+               })
+               .catch(e=>console.log(e))
+               
+      }
+  },
+};
 </script>
 <style>
 
