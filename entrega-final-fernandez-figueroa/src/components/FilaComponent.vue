5d590baa1f54433e218c2e1f9bf248718a00aaa7
Jona Fernandez <jonaafernandez02@gmail.com>
2021-02-19 20:00:34 -0300

    0 cambios

--- entrega-final-fernandez-figueroa/src/components/FilaComponent.vue
+++ entrega-final-fernandez-figueroa/src/components/FilaComponent.vue
@@ -7,22 +7,31 @@
             <td>{{persona.Email}}</td>
             <td>{{persona.Tema}}</td>
             <td>{{persona.Anio}}</td>
-            <td class="d-flex justify-content-around table-dark"> <button>Editar</button> <button>Eliminar</button> </td> 
+            <td class="d-flex justify-content-around table-dark"> <button class="bg-dark text-warning">Editar</button> <button class="bg-dark text-warning" @click="eliminarFila(persona.id)">Eliminar</button> </td> 
         </tr> 
 
    
 </template>
 <script>
+    /* import axios from "axios" */
     export default {
         props: {
             persona: {
                 type: Object,
-                required: true
+                required: true,
+                personas: []
             }
 
+        },
+        methods: {
+            eliminarFila() {
+                console.log("Id: ",
+                    this.persona.id, "NOMBRE: ",
+                    this.persona.Nombre);
+            }
         }
     }
 </script>
-<style lang="">
+<style>
 
 </style>
--- entrega-final-fernandez-figueroa/src/components/Noticias.vue
+++ entrega-final-fernandez-figueroa/src/components/Noticias.vue
@@ -24,6 +24,7 @@
                     
             </table>
         </div>
+        
     </div>
 </template>
 
